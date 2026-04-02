433c501f9f74f9d996de53aae4269709792bbd85
Serly Nazarian <serlynazarian@gmail.com>
2026-04-02 15:07:23 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { MyCard, MyDivider } from "my-ui-lib";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MyCard>
+        <div>dajsjdas</div>
+        <MyDivider />
+      </MyCard>
     </div>
   );
 }
